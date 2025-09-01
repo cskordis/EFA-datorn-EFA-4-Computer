4daed9832a59765e988c06e754643ac3535ed46b
Costas Skordis <conneth@live.com>
2025-08-31 20:54:39 -0500

    Add files via upload

---
+++ EFA-4 Auto Keyer/Firmware/EFA4Loader/userconfig.h
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////////////////////
+/*                                                             */
+/*                                                             */
+/*          Configure your screen settings here                */
+/*                                                             */
+/*                                                             */
+/////////////////////////////////////////////////////////////////
+
+
+#define VERSION "EFA 4 Auto Key"
+#define OLED1306      1            /* Defines if you are using a 128x64 or 128x32 OLED screen */
+#define SerialDebug   1            /* Enable Serial debug */
+#define ScrollLogo    0            /* Scroll logo if defined and if set to 0 center logo or 0 - 8 to scroll right to left */ 
+#define InvertInKey   1            /* Invert signal for inkey */
+#define DataDelay     5            /* Delay for data */
+#define InKeyDelay    10           /* Delay InKey pulse*/
